f3992280284d4a1a0c175a43a1c261cb55e62777
Andi <cska8006@gmail.com>
2025-04-04 01:11:08 +0200

    total_admin_animal_crud

--- src/app/admin.animals/admin.animals.component.ts
+++ src/app/admin.animals/admin.animals.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../admin.service';
 
 @Component({
@@ -6,30 +6,71 @@ import { AdminService } from '../admin.service';
   templateUrl: './admin.animals.component.html',
   styleUrls: ['./admin.animals.component.css']
 })
-export class AdminAnimalsComponent {
-  newAnimal = {
-    name: '',
-    type: '',
-    size: '',
-    date_of_birth: '',
-    date_of_admission: '',
-    description: '',
-    gender: '',
-    image: '',
-    adopted: false
+export class AdminAnimalsComponent implements OnInit {
+  animals: any[] = [];
+  newAnimal: any = {
+      name: '',
+      type: '',
+      size: '',
+      date_of_birth: '',
+      date_of_admission: '',
+      description: '',
+      gender: '',
+      image: '',
+      adopted: false
   };
 
   constructor(private adminService: AdminService) {}
 
+  ngOnInit() {
+      this.loadAnimals();
+  }
+
+  loadAnimals() {
+    this.adminService.getAnimals().subscribe((response: any) => {
+        console.log('Válasz a backend-től:', response);  // Ellenőrizd a válasz formátumát
+        if (response.success && Array.isArray(response.data)) {
+            this.animals = response.data.map((animal: any) => ({ ...animal, edited: false }));
+        } else {
+            console.error('A válasz nem tartalmazza a várt adatokat:', response);
+        }
+    }, error => {
+        console.error('Hiba történt az állatok betöltésekor:', error);
+    });
+  }
+
   onSubmit() {
-    this.adminService.addAnimal(this.newAnimal).subscribe(
-      (response) => {
-        console.log('Állat hozzáadva:', response);
-        // További műveletek, például a sikeres hozzáadás utáni visszajelzés.
-      },
-      (error) => {
-        console.error('Hiba az állat hozzáadásakor:', error);
+      this.adminService.addAnimal(this.newAnimal).subscribe(() => {
+          this.loadAnimals();
+          this.newAnimal = {
+              name: '',
+              type: '',
+              size: '',
+              date_of_birth: '',
+              date_of_admission: '',
+              description: '',
+              gender: '',
+              image: '',
+              adopted: false
+          };
+      });
+  }
+
+  onEdit(animal: any) {
+      animal.edited = true;
+  }
+
+  updateAnimal(animal: any) {
+      this.adminService.updateAnimal(animal).subscribe(() => {
+          animal.edited = false;
+      });
+  }
+
+  deleteAnimal(id: number) {
+      if (confirm('Biztosan törölni szeretnéd?')) {
+          this.adminService.deleteAnimal(id).subscribe(() => {
+              this.animals = this.animals.filter(a => a.id !== id);
+          });
       }
-    );
   }
 }
